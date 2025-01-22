d3b2cb732c2f3f733f6c4f77434fdee0d018c5d1
toni-zitoni <devin.winters77@gmail.com>
2025-01-21 19:58:01 -0700

    Fixed problem in Player.gd and added tileset.

--- Player.gd
+++ Player.gd
@@ -33,7 +33,7 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir := Input.get_vector("left", "right", "up", "down")
-	var direction := (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
